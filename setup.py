704643f0d19a1bdee79d425548d31d9233dade65
Konstantin Malanchev <hombit@gmail.com>
2018-02-15 10:45:01 +0300

    Add minimal setup.py
    Fix #2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='theSNisRight',
+    license='MIT',
+    author='Konstantin Malanchev',
+    author_email='malanchev@physics.msu.ru',
+    py_modules=['curves'],
+)
