31217c77447d1f3e96468c0f7a863a8e9f1ac605
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2018-06-06 18:34:47 +0300

    Produce tsne_pr_?.csv from gri_pr.csv

--- tsne.py
+++ tsne.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
+import sys
 import numpy as np
 import pandas as pd
 from sklearn.manifold import TSNE
+
+_, output, dim = sys.argv
+
+if output is None:
+	raise ValueError()
+
+if dim is None:
+	dim = 9
+
+dim = int(dim)
 
 def do_metric(X1, X2):
 	mask = np.invert((X1 == 0.0) | (X2 == 0.0))
@@ -19,7 +30,7 @@ norm = np.amax(data, axis=0)
 
 data = data / norm
 
-n_features = 9
+n_features = dim
 method = 'exact'
 
 t = TSNE(n_components=n_features, method=method, metric=do_metric)
@@ -28,4 +39,4 @@ new_data = t.fit_transform(data)
 sn_name = df[['SN']]
 
 new_df = pd.concat([sn_name.reset_index(drop=True), pd.DataFrame(new_data)], axis=1)
-new_df.to_csv("tsne_pr.csv")
+new_df.to_csv(output)
