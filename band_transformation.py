2f5fa582e264f6f5547274ae92eca0d2bcd1f51d
Konstantin Malanchev <hombit@gmail.com>
2018-06-07 10:24:54 +0300

    Add fill_value for negative fluxes

--- band_transformation.py
+++ band_transformation.py
@@ -4,7 +4,7 @@ import numpy as np
 from multistate_kernel.util import MultiStateData
 
 
-def band_transformation(msd, a, b, new_bands, old_bands=None):
+def band_transformation(msd, a, b, new_bands, old_bands=None, fill_value=np.nan):
     if old_bands is None:
         old_bands = tuple(msd.keys())
     assert a.shape == (len(new_bands), len(old_bands))
@@ -13,7 +13,7 @@ def band_transformation(msd, a, b, new_bands, old_bands=None):
     for i, x in enumerate(msd.odict[old_bands[0]].x):
         old_flux = np.array([msd.odict[band][i].y for band in old_bands])
         if np.any(old_flux <= 0):
-            new_flux = np.full_like(b, np.nan)
+            new_flux = np.full_like(b, fill_value)
         else:
             old_magn = -2.5 * np.log10(old_flux)
             new_magn = np.dot(a, old_magn) + b
@@ -25,14 +25,14 @@ def band_transformation(msd, a, b, new_bands, old_bands=None):
     return MultiStateData.from_state_data(flux)
 
 
-def VR_to_gri(msd):
+def VR_to_gri(msd, **kwargs):
     a = np.array([[1.9557, -0.9557],
                   [0.6965,  0.3035],
                   [1.7302, -0.7302]])
     b = np.array([-0.0853, 0.0688, 0.3246])
     new_bands = ('g', 'r', 'i')
     old_bands = ('V', 'R')
-    return band_transformation(msd, a, b, new_bands, old_bands=old_bands)
+    return band_transformation(msd, a, b, new_bands, old_bands=old_bands, **kwargs)
 
 
 if __name__ == '__main__':
