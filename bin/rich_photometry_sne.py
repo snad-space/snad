43254a141578ec0baa74c77d04c0d8f18f553de5
Konstantin Malanchev <hombit@gmail.com>
2018-07-05 20:08:00 +0300

    Add information about spectral classification

--- bin/rich_photometry_sne.py
+++ bin/rich_photometry_sne.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+import copy
 import multiprocessing
 import os
+from collections import OrderedDict
 from functools import partial
 
-from thesnisright import SNFiles, OSCCurve
+from thesnisright import SNFiles, OSCCurve, get_anderson_sne, get_cccp_sne, get_pantheon_sne_suffixes, get_sanders_sne
 from thesnisright.load.snfiles import all_snad_objects
 from thesnisright.load.curves import EmptyPhotometryError, NoPhotometryError
 
@@ -12,6 +14,22 @@ from thesnisright.load.curves import EmptyPhotometryError, NoPhotometryError
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
 SN_SET_ATTRS = ('claimedtype', 'redshift', 'alias')
 SN_NUMBER_ATTRS = ('spectrum_count',)
+
+SAMPLE_DATA_PATH = os.path.join(PROJECT_ROOT, 'data/spec_class_samples')
+SNE_FROM_SAMPLES = OrderedDict((
+    ('anderson', get_anderson_sne(os.path.join(SAMPLE_DATA_PATH, 'anderson_sne.csv'))),
+    ('cccp', get_cccp_sne(os.path.join(SAMPLE_DATA_PATH, 'cccp_sne.csv'))),
+    ('pantheon', get_pantheon_sne_suffixes(os.path.join(SAMPLE_DATA_PATH, 'pantheon_sne.dat'))),
+    ('sanders', get_sanders_sne(os.path.join(SAMPLE_DATA_PATH, 'sanders_sne.tsv'))),
+))
+SN_IN_SAMPLE_ATTRS = tuple('in_{}'.format(sample) for sample in SNE_FROM_SAMPLES)
+
+
+def in_sample(name, sample):
+    for sn in SNE_FROM_SAMPLES[sample]:
+        if name.endswith(sn):
+            return True
+    return False
 
 
 def is_rich(file_path, bands):
@@ -36,6 +54,10 @@ def is_rich(file_path, bands):
         attrs_dump.append(';'.join(map(str, getattr(curve, attr))))
     for attr in SN_NUMBER_ATTRS:
         attrs_dump.append(str(getattr(curve, attr)))
+    in_samples = tuple(in_sample(curve.name, sample) for sample in SNE_FROM_SAMPLES)
+    attrs_dump.extend(str(int(x)) for x in in_samples)
+    is_spec_class = any((curve.spectrum_count > 3,) + in_samples)
+    attrs_dump.append(str(int(is_spec_class)))
     return ','.join(attrs_dump) + '\n'
 
 
@@ -52,7 +74,9 @@ def main():
         os.makedirs(data_path, exist_ok=True)
         csv_path = os.path.join(data_path, 'min3obs_{}.csv'.format(bands.replace("'", '_pr')))
         with open(csv_path, 'w') as f:
-            f.write('Name,{}\n'.format(','.join(SN_SET_ATTRS + SN_NUMBER_ATTRS)))
+            f.write('Name,{},spec_class\n'.format(
+                ','.join(SN_SET_ATTRS + SN_NUMBER_ATTRS + SN_IN_SAMPLE_ATTRS))
+            )
             f.writelines(lines)
 
 
